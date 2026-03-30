a82619bf29be9e5e557f9460b0f760a961362593
PedroCacador <gamermastermg26@gmail.com>
2026-03-30 15:38:55 -0300

    Remove endpoints não utilizados e importação desnecessária no arquivo index.ts

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
-import { error } from "node:console";
 
 const app = express();
 
@@ -16,28 +15,3 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na na porta ${PORT}`);
 });
-
-// Endpoints - Nas próximas entregas devo organizar melhor em pastas
-app.get("/status", (req: Request, res: Response)=>{
-    res.send("Olhos sempre abertos.")
-});
-
-app.post("/agente", (req: Request, res: Response) =>{
-    const {nome, codinome} = req.body;
-
-    if(nome == codinome){
-        return res.status(401).send({ error: "Proibido nome e codinome iguais!"});
-    }
-
-});
-
-app.get("/objetos", (req: Request, res: Response) => {
-  const objetos = [
-    { id: 1, nome: "Detector de membrana" },
-    { id: 2, nome: "Jaqueta do Veríssimo" }
-  ];
-
-  res.send(objetos);
-});
-
-
